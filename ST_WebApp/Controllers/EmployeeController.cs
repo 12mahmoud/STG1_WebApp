9d146f6df4f6a22ce07849931b96893b4d17b5ca
Mahmoud Tareq <mahmoudtarek.1335@gmail.com>
2023-08-01 15:38:40 +0300

    Add project files.

---
+++ ST_WebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ST_WebApp.Models;
+
+namespace ST_WebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult GetById(int id)
+        {
+            Employee employee = SampleData.Employees.FirstOrDefault(e => e.Id == id);
+
+            if (employee == null)
+            {
+                ViewBag.EmployeeNotFound = true;
+                return View();
+            }
+
+            return View(employee);
+        }
+
+
+
+        // Action to get all employees
+        public IActionResult GetAll()
+        {
+            return View(SampleData.Employees);
+        }
+
+    }
+}
---
+++ ST_WebApp/Models/SampleData.cs
@@ -0,0 +1,49 @@
+﻿namespace ST_WebApp.Models
+{
+    public class SampleData
+    {
+        public static List<Employee> Employees { get; } = new List<Employee>
+        {
+            new Employee
+            {
+                Id = 1,
+                Name = "Mahmoud Sharnoby",
+                Age = 21,
+                Salary = 15000,
+                Address = "menof",
+                Email = "Sharnoby@gmail.com"
+            },
+            new Employee
+            {
+                Id = 2,
+                Name = "Adham Diab",
+                Age = 25,
+                Salary = 5000,
+                Address = "Giza",
+                Email = "Adham22@gmsil.com"
+            },
+            new Employee
+            {
+                Id = 3,
+                Name = "Mohamed Karim ",
+                Age = 36,
+                Salary = 2500,
+                Address = "New Cairo",
+                Email = "Mokarim@gmail.com"
+            },
+                        new Employee
+            {
+                Id = 4,
+                Name = "Maria Ali",
+                Age = 21,
+                Salary = 7500,
+                Address = "New cairo",
+                Email = "MariaAli@yahoo.com"
+            },
+
+
+
+
+        };
+    }
+}
